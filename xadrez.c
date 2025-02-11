7a2e8ce039e0e68fdcabf43e9e2c8c37a9c0d2c9
Ade-hrq <Adeilsonhrc@gmail.com>
2025-02-11 16:27:45 +0000

    movimentação de peças

--- xadrez.c
+++ xadrez.c
@@ -5,6 +5,24 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
+//O for insere os valores da variavel e faz a funcao verdadeira para aparecer
+    for(int casas = 0; casas <= 5; casas++){
+//Aqui ja se entra com um if, para para exibir apenas um numero em especifico
+    if(casas == 5){
+    printf("\n --MOVENDO PECAS--\n");
+    printf("\n** Bispo **\n");
+    printf("\nMovendo bispo %d casas para diagonal direita\n", casas);
+    printf("\n** Torre **\n");
+    printf("\nMovendo torre %d casas para direita\n", casas);
+    }
+//Como a rainha tinha um valor diferente, foi feito outro for semelhante
+//Porem com o valor diferente 
+} for (int c = 0; c <=8; c++){
+    if (c == 8){
+        printf ("\n** Rainha **\n");
+        printf("\nMovendo rainha %d casas para esquerda\n", c);
+    }
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
