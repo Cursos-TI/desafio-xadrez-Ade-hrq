06c8fa7b3101c5401a080e55901ca5e509e1791b
Ade-hrq <Adeilsonhrc@gmail.com>
2025-02-16 01:24:05 +0000

    Loops aninhados

--- xadrez.c
+++ xadrez.c
@@ -4,52 +4,87 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
 
-//O for insere os valores da variavel e faz a funcao verdadeira para aparecer
-   int casas;
-
-//Titulo do que está acotecendo
-    printf("\n --MOVENDO PECAS--\n");
-
-//primeiro for para bispo
-    printf("\n** Bispo **\n");
-    for(casas = 1; casas <= 5; casas++)
-    {
-    printf("\n %d casas para diagonal direita\n", casas);
+void bispo(int casa){
+    if(casa > 5){
+     bispo(casa + 1);
+     printf("%d casas para diagonal direita");
     }
+ }
+     void torre(int casa){
+         if(casa > 5){
+          torre(casa + 1);
+          printf("%d casas para diagonal direita");
+     }
+ }
+ 
+ void rainha(int casa){
+     if(casa > 5){
+     rainha(casa + 1);
+     printf("%d casas para diagonal direita");
+          
+ }
+ 
+ void cavalo(int casa){
+     if(casa > 5){
+     cavalo(casa + 1);
+     printf("%d casas para diagonal direita");
+              
+ }
+ }
+ 
+ 
+ 
+ }
+ 
+ 
+ 
+ int main() {
+ 
+ //O for insere os valores da variavel e faz a funcao verdadeira para aparecer
+    int casas;
+ 
+ //Titulo do que está acotecendo
+     printf("\n --MOVENDO PECAS--\n");
+ 
+ //primeiro for para bispo
+ printf("\n** Bispo **\n");
+ for(int casas = 1; casas <= 5; casas++)
+ {
+ printf("\n %d casas para diagonal direita\n", casas);
+ }
+     
+ //Segundo for para torre
+     printf("\n** Torre **\n");
+     for(int casas = 1; casas <= 5; casas++)
+     {
+     printf("\n %d casas para direita\n", casas);
+     }
+ 
+ //Terceiro for para rainha
+ 
+     printf ("\n** Rainha **\n");
+     for (int casas = 1; casas <= 8; casas++)
+     {
+         printf("\n %d casas para esquerda\n", casas);
+     }
+  // Quarto for para cavalo, usando loops aninhados
+     printf("\n** Cavalo **\n");
+ 
+ //Ele vai andar uma casa pra direita, quando o for interno for completo
+     for(int casas = 1; casas <= 1; casas++)
+     {
+ 
+ //O for interno vai andar 2x para frente a partir do 1
+    for(int casas = 1; casas <= 2; casas++){
+     printf("\n %d casas para frente\n", casas);
+    }
+ //O printf do for externo, precisa ficar depois do interno
+ //Para ser exibido depois de ser exibido o for interno 2x
+ //Fazendo assim o formato em L
+         printf("\n %d casa para direita\n", casas);
+     }
     
-//Segundo for para torre
-    printf("\n** Torre **\n");
-    for(int casas = 1; casas <= 5; casas++)
-    {
-    printf("\n %d casas para direita\n", casas);
-    }
-
-//Terceiro for para rainha
-
-    printf ("\n** Rainha **\n");
-    for (int casas = 1; casas <= 8; casas++)
-    {
-        printf("\n %d casas para esquerda\n", casas);
-    }
- // Quarto for para cavalo, usando loops aninhados
-    printf("\n** Cavalo **\n");
-
-//Ele vai andar uma casa pra direita, quando o for interno for completo
-    for(int casas = 1; casas <= 1; casas++)
-    {
-
-//O for interno vai andar 3x para frente a partir do 1
-        for(int casas = 1; casas <= 3; casas++)
-        {
-            printf("\n %d casas para frente\n", casas);
-        }
-//O printf do for externo, precisa ficar depois do interno
-//Para ser exibido depois de ser exibido o for interno 3x
-//Fazendo assim o formato em L
-        printf("\n %d casa para direita\n", casas);
-    }
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
